b2467120ae284a496bdbcfd07506e3da37a9b72a
怀政 <apokar@163.com>
2017-09-04 12:57:47 +0800

    9-4 查询结果ok --使用urllib.quote解决中文参数问题

--- test.py
+++ test.py
@@ -4,6 +4,7 @@
 import re
 import os
 import mechanize
+import urllib
 from bs4 import BeautifulSoup
 import requests
 
@@ -34,44 +35,25 @@ def re_findall(pattern, html):
 
 
 def do_search_keyword():
-    # keyword = raw_input('请输入企业名称、人名、产品名称或其它关键词 ：')
-    keyword = '北京智齿'
+    keyword = raw_input('请输入企业名称、人名、产品名称或其它关键词 ：')
+    # keyword = u'user_input'
+    # keyword = '北京智齿'
     # url='https://www.tianyancha.com/search?key=' +str(keyword)+ '&checkFrom=searchBox'
-    url = 'https://www.tianyancha.com/search?key='+keyword+'&checkFrom=searchBox'
+    url = 'https://www.tianyancha.com/search?key='+urllib.quote(keyword)+'&checkFrom=searchBox'
 
-    br = mechanize.Browser()
-    br.addheaders = [
-        ('Accept', 'application/json, text/javascript, */*; q=0.01') \
-        , ('Accept-Encoding', 'gzip, deflate, br') \
-        , ('Accept-Language', 'zh-CN,zh;q=0.8,en;q=0.6') \
-        , ('Connection', 'keep-alive') \
-        , ('Cookie',
-           'TYCID=8c420960894b11e79bb7cf4adc554d53; uccid=baeee58fe4d1d697092e61f6525e8719;ssuid=6805162414; RTYCID=d6928b5fe88243489ad83678134a5e06; tyc-user-info=%257B%2522token%2522%253A%2522eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODU1MTk4NjYyNSIsImlhdCI6MTUwNDE0NDg1MCwiZXhwIjoxNTE5Njk2ODUwfQ.ChipyAlCl7j08qBgYW8nsR46mIs91UTI80IftelNgjDf59KZ6JWWQm9BDonBKsl5UKyO44PtnzV1olf-RjUXMQ%2522%252C%2522integrity%2522%253A%25220%2525%2522%252C%2522state%2522%253A%25220%2522%252C%2522vnum%2522%253A%25220%2522%252C%2522onum%2522%253A%25220%2522%252C%2522mobile%2522%253A%252218551986625%2522%257D; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODU1MTk4NjYyNSIsImlhdCI6MTUwNDE0NDg1MCwiZXhwIjoxNTE5Njk2ODUwfQ.ChipyAlCl7j08qBgYW8nsR46mIs91UTI80IftelNgjDf59KZ6JWWQm9BDonBKsl5UKyO44PtnzV1olf-RjUXMQ; aliyungf_tc=AQAAAIrjRh3IugIAlaRf3sGFCOfYdqPV; csrfToken=sWaDXU2nJB0QKqUAV0D8XSzN; bannerFlag=true; OA=e+gaygcWZ84c32kAIH2c9feRsbYPaO9so59OI1hKLF4bER8rcljLYdw2EDQSPJZc; _csrf=HzN2RRmaxND842F/ICyCpQ==; _csrf_bk=e73360ec0593474650ab26dac3b35170; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1503634325,1504143041,1504148840; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1504176400') \
-        , ('Host', 'www.tianyancha.com') \
-        , ('Referer', 'https://www.tianyancha.com/search?key='+keyword+'&checkFrom=searchBox') \
-        , ('User-Agent',
-           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36') \
-        , ('X-Requested-With', 'XMLHttpRequest') \
-        ]
+    headers = {
+        'Cookie': 'TYCID=8c420960894b11e79bb7cf4adc554d53; uccid=baeee58fe4d1d697092e61f6525e8719; ssuid=6805162414; aliyungf_tc=AQAAAOsOUQId4QcAlaRf3mqAPMUDMG/2; csrfToken=S2nttCpDrr4WCbvLkQRClEUt; bannerFlag=true; _csrf=i6MDX6NEr+KEpAxRAcWeaA==; OA=cxAohDKsDZv4yk4sQ70GtLb5KtPEhEnIp/d25AgGeuU=; _csrf_bk=76b9aab25bdab0db8930d22ee4171984; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1503634325,1504143041,1504148840,1504245847; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1504490343',
+        'Host': 'www.tianyancha.com',
+        'Referer': 'https://www.tianyancha.com/',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla / 5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+    }
+    html = requests.get(url, headers=headers)
+    soup = BeautifulSoup(html.text, 'lxml')
 
-    html = br.open(url).read()
-    soup = BeautifulSoup(html, 'lxml')
-
-    print soup.prettify()
-
-    # for i in range(1,21):
-    #
-    #     txt = soup.select(
-    #         '#web-content > div > div > div > div.col-9.search-2017-2.pr10.pl0 > div.b-c-white.search_result_container > div:nth-of-type('+str(i)+')  > div.search_right_item > div.row.pb5 > div.col-xs-10.search_repadding2.f18 > a')[
-    #         0]['href']
-    #     print txt
-    #     # if txt:
-        #     print 'you'
-        # else:
-        #     print 'no'
-    # for result_list in result_lists :
-    #     for i in result_list:
-    #         print i
-
+    # print soup.prettify()
+    urls= soup.select('#web-content > div > div > div > div.col-9.search-2017-2.pr10.pl0 > div.b-c-white.search_result_container > div > div.search_right_item > div.row.pb5 > div.col-xs-10.search_repadding2.f18 > a')
+    for url in urls:
+        print url['href']
 if __name__=='__main__':
     do_search_keyword()
