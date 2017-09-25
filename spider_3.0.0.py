9a9c51e55c4a38e3d321d7084b36926e2651f392
huaiz <apokar@163.com>
2017-09-25 14:44:24 +0800

    3.0 基本流程可以走一遍,接下来要添加代理

---
+++ spider_3.0.0.py
@@ -0,0 +1,240 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+import os
+from multiprocessing import Pool
+import json
+import hashlib
+import datetime
+import MySQLdb
+import time
+import csv
+from bs4 import BeautifulSoup
+import requests
+import urllib
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# 禁用安全请求警告
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+static_js_code = """
+var ne = "2633141825201321121345332721524273528936811101916293117022304236|1831735156281312241132340102520529171363214283321272634162219930|2332353860219720155312141629130102234183691124281413251227261733|2592811262018293062732141927100364232411333831161535317211222534|9715232833130331019112512913172124126035262343627321642220185148|3316362031032192529235212215274341412306269813312817111724201835|3293412148301016132183119242311021281920736172527353261533526224|3236623313013201625221912357142415851018341117262721294332103928|2619332514511302724163415617234183291312001227928218353622321031|3111952725113022716818421512203433241091723133635282932601432216";
+var base64chars = "abcdefghijklmnopqrstuvwxyz1234567890-~!";
+var _0x4fec = "f9D1x1Z2o1U2f5A1a1P1i7R1u2S1m1F1,o2A1x2F1u5~j1Y2z3!p2~r3G2m8S1c1,i3E5o1~d2!y2H1e2F1b6`g4v7,p1`t7D3x5#w2~l2Z1v4Y1k4M1n1,C2e3P1r7!s6U2n2~p5X1e3#,g4`b6W1x4R1r4#!u5!#D1f2,!z4U1f4`f2R2o3!l4I1v6F1h2F1x2!,b2~u9h2K1l3X2y9#B4t1,t5H1s7D1o2#p2#z1Q3v2`j6,r1#u5#f1Z2w7!r7#j3S1";
+rs_decode = function(e) {
+    return ne.split("|")[e]
+};
+    var r = t+"";
+    r = r.length > 1 ? r[1] : r;
+    for (var i = rs_decode(r), o = _0x4fec.split(",")[r], a = [], s = 0, u = 0; u < o.length; u++) {
+        if ("`" != o[u] && "!" != o[u] && "~" != o[u] || (a.push(i.substring(s, s + 1)), s++), "#" == o[u] && (a.push(i.substring(s, s + 1)), a.push(i.substring(s + 1, s + 3)), a.push(i.substring(s + 3, s + 4)), s += 4), o.charCodeAt(u) > 96 && o.charCodeAt(u) < 123) for (var l = o[u + 1], c = 0; c < l; c++) a.push(i.substring(s, s + 2)),
+        s += 2;
+        if (o.charCodeAt(u) > 64 && o.charCodeAt(u) < 91) for (var l = o[u + 1], c = 0; c < l; c++) a.push(i.substring(s, s + 1)),
+        s++
+    }
+    rsid = a;
+for (var chars = "",  i = 0; i < rsid.length; i++) chars += base64chars[rsid[i]];
+for (var fxck = wtf.split(","), fxckStr = "", i = 0; i < fxck.length; i++) fxckStr += chars[fxck[i]];
+var utm = fxckStr;
+console.log("{\\"utm\\":\\""+utm+"\\",\\"ssuid\\":\\""+Math.round(2147483647 * Math.random()) * (new Date).getUTCMilliseconds() % 1e10+"\\"}")
+phantom.exit();
+"""
+
+conn = MySQLdb.connect(host="localhost", user="root", passwd="root", db="tianyancha", charset="utf8")
+cursor = conn.cursor()
+
+def execCmd(cmd):
+    r = os.popen(cmd)
+    text = r.read()
+    r.close()
+    return (text)
+
+
+# def re_findall(pattern, html):
+#     if re.findall(pattern, html, re.S):
+#         return re.findall(pattern, html, re.S)
+#     else:
+#         return 'N'
+
+
+def do_search_keyword(keyword):
+    #    北京百度网讯科技有限公司
+    # keyword = raw_input('请输入企业名称、人名、产品名称或其它关键词 ：')
+    url = 'https://www.tianyancha.com/search?key=' + urllib.quote(keyword) + '&checkFrom=searchBox'
+    print url
+    headers = {
+        'Cookie': 'TYCID=8c420960894b11e79bb7cf4adc554d53; uccid=baeee58fe4d1d697092e61f6525e8719; ssuid=6805162414; aliyungf_tc=AQAAAOsOUQId4QcAlaRf3mqAPMUDMG/2; csrfToken=S2nttCpDrr4WCbvLkQRClEUt; bannerFlag=true; _csrf=i6MDX6NEr+KEpAxRAcWeaA==; OA=cxAohDKsDZv4yk4sQ70GtLb5KtPEhEnIp/d25AgGeuU=; _csrf_bk=76b9aab25bdab0db8930d22ee4171984; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1503634325,1504143041,1504148840,1504245847; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1504490343',
+        'Host': 'www.tianyancha.com',
+        'Referer': 'https://www.tianyancha.com/',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla / 5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+    }
+    html = requests.get(url, headers=headers)
+
+    urls_result = re.findall('<div class="search_right_item"><div><a href="(.*?)"',html.text,re.S)
+    print urls_result
+    return urls_result
+
+# 得到soup
+def get_page(url):
+    headers = {
+        'Cookie': 'TYCID=8c420960894b11e79bb7cf4adc554d53; uccid=baeee58fe4d1d697092e61f6525e8719; ssuid=6805162414; aliyungf_tc=AQAAAOsOUQId4QcAlaRf3mqAPMUDMG/2; csrfToken=S2nttCpDrr4WCbvLkQRClEUt; bannerFlag=true; _csrf=i6MDX6NEr+KEpAxRAcWeaA==; OA=cxAohDKsDZv4yk4sQ70GtLb5KtPEhEnIp/d25AgGeuU=; _csrf_bk=76b9aab25bdab0db8930d22ee4171984; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1503634325,1504143041,1504148840,1504245847; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1504490343',
+        'Host': 'www.tianyancha.com',
+        'Referer': 'https://www.tianyancha.com/',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla / 5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+    }
+
+    html = requests.get(url, headers=headers)
+    soup = BeautifulSoup(html.text, 'lxml')
+    return soup
+
+
+## 获取公司名和cid
+def basic_info(soup):
+    global company_name
+
+    company_name = soup.find_all('span',class_="f18 in-block vertival-middle")[0].text
+    # '#company_web_top > div.companyTitleBox55.pt30.pl30.pr30 > div.company_header_width.ie9Style > div:nth-child(1) > span.f18.in-block.vertival-middle.sec-c2'
+    # '#company_web_top > div.companyTitleBox55.pt30.pl30.pr30 > div.company_header_width.ie9Style > div > span.f18.in-block.vertival-middle.sec-c2'
+
+
+## 工商信息
+def get_business_info(soup):
+    # 注册资本
+    registered_capital = soup.select(
+        '#_container_baseInfo > div > div.baseInfo_model2017 > table > tbody > tr > td:nth-of-type(2) > div:nth-of-type(1) > div.pb10 > div')[0].text
+    print registered_capital
+
+    # 注册时间
+    registration_time = soup.select(
+        '#_container_baseInfo > div > div.baseInfo_model2017 > table > tbody > tr > td:nth-of-type(2) > div.new-border-bottom.pt10 > div.pb10 > div')[0].text
+    print registration_time
+
+    # 企业状态
+    company_status = soup.select(
+        '#_container_baseInfo > div > div.baseInfo_model2017 > table > tbody > tr > td:nth-of-type(2) > div:nth-of-type(3) > div:nth-of-type(2) > div')[0].text
+    print company_status
+
+    # 工商注册号
+    business_registration_number = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(1) > td:nth-of-type(2)')[0].text
+    print business_registration_number
+
+    # 组织机构代码
+    organization_code = soup.select(
+
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(1) > td:nth-of-type(4)')[0].text
+    print organization_code
+
+    # 统一信用代码
+    uniform_credit_code = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(2) > td:nth-of-type(2)')[0].text
+    print uniform_credit_code
+
+    # 企业类型
+    enterprise_type = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(2) > td:nth-of-type(4)')[0].text
+    print enterprise_type
+
+    # 纳税人识别号
+    taxpayer_identification_number = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(3) > td:nth-of-type(2)')[0].text
+    print taxpayer_identification_number
+
+    # 行业
+    industry = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(3) > td:nth-of-type(4)')[0].text
+    print industry
+
+    # 营业期限
+    business_term = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(4) > td:nth-of-type(2) > span')[0].text
+    print business_term
+
+    # 核准日期
+    approval_date = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(4) > td:nth-of-type(4)')[0].text
+    print approval_date
+
+    # 登记机关
+    registration_authority = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(5) > td:nth-of-type(2)')[0].text
+    print registration_authority
+
+    # 注册地址
+    registered_address = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(5) > td:nth-of-type(4)')[0].text
+    print registered_address
+
+    # 英文名称
+    english_name = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(6) > td:nth-of-type(2)')[0].text
+    print english_name
+
+    # 经营范围
+    scope_of_business = soup.select(
+        '#_container_baseInfo > div > div.base0910 > table > tbody > tr:nth-of-type(7) > td:nth-of-type(2) > span > span > span.js-full-container')[0].text
+    print scope_of_business
+
+    return (
+        'insert into tyc_business_info values("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")' % (
+            keyword[0], company_name, registered_capital,registration_time,company_status,business_registration_number,organization_code,uniform_credit_code,
+    enterprise_type,taxpayer_identification_number,industry,business_term,approval_date,registration_authority,registered_address,english_name,scope_of_business,
+            str(datetime.datetime.now()), str(datetime.datetime.now())[:10]
+        ))
+
+
+## 主要人员
+def staff_info(soup):
+    num = soup.select('#nav-main-staffCount > span')[0].text
+    print num
+    for i in range(1, int(num) + 1):
+        position = soup.select('#_container_staff > div > div > div:nth-of-type(' + str(
+            i) + ') > div > div.in-block.f14.new-c5.pt9.pl10.overflow-width.vertival-middle > span')[0].text
+        name = soup.select('#_container_staff > div > div > div:nth-of-type(' + str(i) + ') > div > a')[0].text
+        ID = soup.select('#_container_staff > div > div > div:nth-of-type(' + str(i) + ') > div > a')[0]['href']
+
+        print position, name, ID
+
+
+## 股东信息
+def shareholder_info(soup):
+    num = soup.select('#nav-main-holderCount > span')[0].text
+    for i in range(1, int(num) + 1):
+        shareholder = soup.select(
+            '#_container_holder > div > table > tbody > tr:nth-of-type(' + str(i) + ') > td:nth-of-type(1) > a')[0].text
+        ratio = \
+            soup.select(
+                '#_container_holder > div > table > tbody > tr:nth-of-type(' + str(i) + ') > td:nth-of-type(2)')[
+                0].text
+        value = \
+            soup.select(
+                '#_container_holder > div > table > tbody > tr:nth-of-type(' + str(i) + ') > td:nth-of-type(3)')[
+                0].text
+        print '股东: ', shareholder, ' 出资比例: ', ratio, ' 认缴出资:', value
+
+if __name__ =='__main__':
+    with open("zhaopin_not_in_jsgsj_basic_info.csv", "r") as csvFile:
+        reader = csv.reader(csvFile)
+        for keyword in reader:
+            if keyword[0].find('company_name') == -1:
+                urls_result=do_search_keyword(keyword[0])
+                for url in urls_result:
+                    print url
+                    global cid
+
+                    soup = get_page(url)
+                    cid = url.split('/')[-1]
+                    basic_info(soup)
+                    print cid
+                    print company_name
+
+                    get_business_info(soup)
+
+                    cursor.execute(get_business_info(soup))
+                    conn.commit()
+                    time.sleep(2)
+
+    csvFile.close()
