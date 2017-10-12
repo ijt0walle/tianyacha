fe88e39a52828c67d6dac49f826d75c675c2884b
huaiz <apokar@163.com>
2017-10-12 16:15:56 +0800

    美化一下代码

--- spider_4.0.py
+++ spider_4.0.py
@@ -16,8 +16,11 @@ import urllib
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+
 # 禁用安全请求警告
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
 static_js_code = """
 var ne = "2633141825201321121345332721524273528936811101916293117022304236|1831735156281312241132340102520529171363214283321272634162219930|2332353860219720155312141629130102234183691124281413251227261733|2592811262018293062732141927100364232411333831161535317211222534|9715232833130331019112512913172124126035262343627321642220185148|3316362031032192529235212215274341412306269813312817111724201835|3293412148301016132183119242311021281920736172527353261533526224|3236623313013201625221912357142415851018341117262721294332103928|2619332514511302724163415617234183291312001227928218353622321031|3111952725113022716818421512203433241091723133635282932601432216";
 var base64chars = "abcdefghijklmnopqrstuvwxyz1234567890-~!";
@@ -41,11 +44,14 @@ console.log("{\\"utm\\":\\""+utm+"\\",\\"ssuid\\":\\""+Math.round(2147483647 * M
 phantom.exit();
 """
 
+
+#清洗数据(去除文本中的<em><br/>
 def detag(html):
     detag = re.subn('<[^>]*>',' ',html)[0]
     return detag
 
 
+#获取代理
 def get_proxy():
     proxy_list = list(set(urllib.urlopen(
         'http://60.205.92.109/api.do?name=3E30E00CFEDCD468E6862270F5E728AF&status=1&type=static').read().split('\n')[
@@ -69,54 +75,9 @@ def execCmd(cmd):
     return (text)
 
 
-def do_search_keyword(keyword):
-    while True:
-        try:
-            proxies2 = get_proxy()
-            url = 'https://www.tianyancha.com/search?key=' + urllib.quote(keyword.encode('utf8')) + '&checkFrom=searchBox'
-            print url
-            headers = {
-                'Cookie': 'TYCID=8c420960894b11e79bb7cf4adc554d53; uccid=baeee58fe4d1d697092e61f6525e8719; ssuid=6805162414; aliyungf_tc=AQAAAOsOUQId4QcAlaRf3mqAPMUDMG/2; csrfToken=S2nttCpDrr4WCbvLkQRClEUt; bannerFlag=true; _csrf=i6MDX6NEr+KEpAxRAcWeaA==; OA=cxAohDKsDZv4yk4sQ70GtLb5KtPEhEnIp/d25AgGeuU=; _csrf_bk=76b9aab25bdab0db8930d22ee4171984; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1503634325,1504143041,1504148840,1504245847; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1504490343',
-                'Host': 'www.tianyancha.com',
-                'Referer': 'https://www.tianyancha.com/',
-                'Upgrade-Insecure-Requests': '1',
-                'User-Agent': 'Mozilla / 5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
-            }
-            html = requests.get(url, proxies=proxies2, headers=headers)
-            # print html.text
-            if html.text.__contains__('https://static.tianyancha.com/wap/images/notFound.png'):
-                urls_result = ['-1']
-                print u'nofound.img'
-            else:
-                urls_result = re.findall('<div class="search_right_item"><div><a href="(.*?)"', html.text, re.S)
-            return urls_result
-            print 'got it'
-            break
-        except Exception,e:
-            print str(e)
-            continue
-
-# 得到soup
-def get_page(url):
-    headers = {
-        'Cookie': 'TYCID=8c420960894b11e79bb7cf4adc554d53; uccid=baeee58fe4d1d697092e61f6525e8719; ssuid=6805162414; aliyungf_tc=AQAAAOsOUQId4QcAlaRf3mqAPMUDMG/2; csrfToken=S2nttCpDrr4WCbvLkQRClEUt; bannerFlag=true; _csrf=i6MDX6NEr+KEpAxRAcWeaA==; OA=cxAohDKsDZv4yk4sQ70GtLb5KtPEhEnIp/d25AgGeuU=; _csrf_bk=76b9aab25bdab0db8930d22ee4171984; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1503634325,1504143041,1504148840,1504245847; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1504490343',
-        'Host': 'www.tianyancha.com',
-        'Referer': 'https://www.tianyancha.com/',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla / 5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
-    }
-
-    html = requests.get(url, proxies=proxies, headers=headers)
-    return html
-
-
-## 获取公司名和cid
-def basic_info(html):
-    global company_name
-    soup = BeautifulSoup(html.text, 'lxml')
-    company_name = soup.find_all('span', class_="f18 in-block vertival-middle sec-c2")[0].text
-    # '#company_web_top > div.companyTitleBox55.pt30.pl30.pr30 > div.company_header_width.ie9Style > div:nth-child(1) > span.f18.in-block.vertival-middle.sec-c2'
-    # '#company_web_top > div.companyTitleBox55.pt30.pl30.pr30 > div.company_header_width.ie9Style > div > span.f18.in-block.vertival-middle.sec-c2'
+#==================================
+#------------各个模块获取------------
+#==================================
 
 
 ## 工商信息
@@ -311,66 +272,6 @@ def shareholder_info(html, cursor):
             str(datetime.datetime.now())[:10]))
 
 
-### 股东信息cookie
-def get_shareholder_cookie(page_no):
-    timestamp = int(time.time() * 1000)
-    head1 = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Host': 'www.tianyancha.com',
-        'Origin': 'https://www.tianyancha.com',
-        'Referer': 'https://www.tianyancha.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'X-Requested-With': 'XMLHttpRequest'
-    }
-    tongji_url = "https://www.tianyancha.com/tongji/" + cid + ".json?_=" + str(timestamp)
-
-    tongji_page = requests.get(tongji_url, headers=head1, proxies=proxies, verify=False)
-
-    cookie = tongji_page.cookies.get_dict()
-    js_code = "".join([chr(int(code)) for code in tongji_page.json()["data"].split(",")])
-
-    token = re.findall(r"token=(\w+);", js_code)[0]
-    utm_code = re.findall("return'([^']*?)'", js_code)[0]
-    t = ord(cid[0])
-
-    fw = open("/Users/huaiz/PycharmProjects/tianyacha/rsid.js", "wb+")
-    fw.write('var t = "' + str(t) + '",wtf = "' + utm_code + '";' + static_js_code)
-    fw.close()
-    phantomResStr = execCmd('phantomjs /Users/huaiz/PycharmProjects/tianyacha/rsid.js')
-    # --print phantomResStr
-    # print "phantomResStr: %s" % phantomResStr
-    phantomRes = json.loads(phantomResStr)
-    ssuid = phantomRes["ssuid"]
-    utm = phantomRes["utm"]
-
-    head2 = {
-        'Host': 'www.tianyancha.com',
-        # 'Referer': 'https://www.tianyancha.com/company/22822',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36',
-        'Accept-Language': 'zh-CN,zh;q=0.8',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, sdch, br',
-        'Cookie': 'ssuid=' + ssuid + '; token=' + token + '; _utm=' + utm + '; aliyungf_tc=' + cookie[
-            "aliyungf_tc"] + '; TYCID=' + cookie["TYCID"] + '; csrfToken=' + cookie["csrfToken"] + '; uccid=' +
-                  cookie["uccid"],
-        'X-Requested-With': 'XMLHttpRequest'
-    }
-
-    url = 'https://www.tianyancha.com/pagination/holder.xhtml?ps=20&pn=' + str(
-        page_no) + '&id=' + cid + '&_=' + str(
-        timestamp - 1)
-
-    resp = requests.get(url, headers=head2, proxies=proxies, verify=False)
-    # print resp
-    html = resp.text
-    return html
-    # quit()
-    # soup2 = BeautifulSoup(html, 'lxml')
-    # return soup2
-
-
 ### 对外投资信息
 def get_invest_info(html, cursor):
     print u'获取对外投资信息  ' + str(datetime.datetime.now())
@@ -429,6 +330,119 @@ def get_invest_info(html, cursor):
              str(datetime.datetime.now()),
              str(datetime.datetime.now())[:10])
         )
+
+
+### 变更记录信息
+def get_change_info(html):
+    print u'获取变更记录信息  ' + str(datetime.datetime.now())
+    if html.text.__contains__('nav-main-changeCount'):
+        soup = BeautifulSoup(html.text, 'lxml')
+        num = soup.select('#nav-main-changeCount > span')[0].text
+        all_page_no = int(num) / 5 + 1
+
+        for i in range(1, int(all_page_no) + 1):
+            soup2 = get_change_cookie(i)
+            res = soup2.select('tr')
+            for x in range(1,len(res)):
+                source = str(res[x])
+                part_one = re_findall('<div>(.*?)</div>',source)
+                part_two = re_findall('<div class="textJustFy changeHoverText">(.*?)</div>',source)
+                change_time = part_one[0]
+                change_projects =part_one[1]
+                before_change =part_two[0]
+                after_change =part_two[1]
+
+                cursor.execute(
+                    'insert into tyc_change_record values ("%s","%s","%s","%s","%s","%s","%s","%s")' %
+                    (keyword,
+                     company_name,
+                     change_time.decode('utf-8'),
+                     change_projects.decode('utf-8'),
+                     detag(before_change).decode('utf-8'),
+                     detag(after_change).decode('utf-8'),
+                     str(datetime.datetime.now()),
+                     str(datetime.datetime.now())[:10])
+                )
+
+    else:
+        print u'没有变更信息的内容'
+        cursor.execute(
+            'insert into tyc_change_record values ("%s","%s","%s","%s","%s","%s","%s","%s")' %
+            (keyword,
+             company_name,
+             'no_change_info',
+             'no_change_info',
+             'no_change_info',
+             'no_change_info',
+             str(datetime.datetime.now()),
+             str(datetime.datetime.now())[:10])
+        )
+
+
+
+# ==================================
+# --------各模块对应cookie获取区-------
+# ==================================
+
+
+### 股东信息cookie
+def get_shareholder_cookie(page_no):
+    timestamp = int(time.time() * 1000)
+    head1 = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Host': 'www.tianyancha.com',
+        'Origin': 'https://www.tianyancha.com',
+        'Referer': 'https://www.tianyancha.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    tongji_url = "https://www.tianyancha.com/tongji/" + cid + ".json?_=" + str(timestamp)
+
+    tongji_page = requests.get(tongji_url, headers=head1, proxies=proxies, verify=False)
+
+    cookie = tongji_page.cookies.get_dict()
+    js_code = "".join([chr(int(code)) for code in tongji_page.json()["data"].split(",")])
+
+    token = re.findall(r"token=(\w+);", js_code)[0]
+    utm_code = re.findall("return'([^']*?)'", js_code)[0]
+    t = ord(cid[0])
+
+    fw = open("/Users/huaiz/PycharmProjects/tianyacha/rsid.js", "wb+")
+    fw.write('var t = "' + str(t) + '",wtf = "' + utm_code + '";' + static_js_code)
+    fw.close()
+    phantomResStr = execCmd('phantomjs /Users/huaiz/PycharmProjects/tianyacha/rsid.js')
+    # --print phantomResStr
+    # print "phantomResStr: %s" % phantomResStr
+    phantomRes = json.loads(phantomResStr)
+    ssuid = phantomRes["ssuid"]
+    utm = phantomRes["utm"]
+
+    head2 = {
+        'Host': 'www.tianyancha.com',
+        # 'Referer': 'https://www.tianyancha.com/company/22822',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, sdch, br',
+        'Cookie': 'ssuid=' + ssuid + '; token=' + token + '; _utm=' + utm + '; aliyungf_tc=' + cookie[
+            "aliyungf_tc"] + '; TYCID=' + cookie["TYCID"] + '; csrfToken=' + cookie["csrfToken"] + '; uccid=' +
+                  cookie["uccid"],
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+
+    url = 'https://www.tianyancha.com/pagination/holder.xhtml?ps=20&pn=' + str(
+        page_no) + '&id=' + cid + '&_=' + str(
+        timestamp - 1)
+
+    resp = requests.get(url, headers=head2, proxies=proxies, verify=False)
+    # print resp
+    html = resp.text
+    return html
+    # quit()
+    # soup2 = BeautifulSoup(html, 'lxml')
+    # return soup2
 
 
 ### 对外投资信息cookie
@@ -504,52 +518,7 @@ def get_invest_cookie(page_no):
             continue
 
 
-### 变更记录信息
-def get_change_info(html):
-    print u'获取变更记录信息  ' + str(datetime.datetime.now())
-    if html.text.__contains__('nav-main-changeCount'):
-        soup = BeautifulSoup(html.text, 'lxml')
-        num = soup.select('#nav-main-changeCount > span')[0].text
-        all_page_no = int(num) / 5 + 1
-
-        for i in range(1, int(all_page_no) + 1):
-            soup2 = get_change_cookie(i)
-            res = soup2.select('tr')
-            for x in range(1,len(res)):
-                source = str(res[x])
-                part_one = re_findall('<div>(.*?)</div>',source)
-                part_two = re_findall('<div class="textJustFy changeHoverText">(.*?)</div>',source)
-                change_time = part_one[0]
-                change_projects =part_one[1]
-                before_change =part_two[0]
-                after_change =part_two[1]
-
-                cursor.execute(
-                    'insert into tyc_change_record values ("%s","%s","%s","%s","%s","%s","%s","%s")' %
-                    (keyword,
-                     company_name,
-                     change_time.decode('utf-8'),
-                     change_projects.decode('utf-8'),
-                     detag(before_change).decode('utf-8'),
-                     detag(after_change).decode('utf-8'),
-                     str(datetime.datetime.now()),
-                     str(datetime.datetime.now())[:10])
-                )
-
-    else:
-        print u'没有变更信息的内容'
-        cursor.execute(
-            'insert into tyc_change_record values ("%s","%s","%s","%s","%s","%s","%s","%s")' %
-            (keyword,
-             company_name,
-             'no_change_info',
-             'no_change_info',
-             'no_change_info',
-             'no_change_info',
-             str(datetime.datetime.now()),
-             str(datetime.datetime.now())[:10])
-        )
-
+### 变更记录信息cookie
 def get_change_cookie(page_no):
     while True:
         try:
@@ -614,6 +583,23 @@ def get_change_cookie(page_no):
                 print u'get_change_cookie 代理失效 换一个试试'
             continue
 
+
+# ==================================
+# ------------主逻辑功能区------------
+# ==================================
+
+
+def get_page(url):
+    headers = {
+        'Cookie': 'TYCID=8c420960894b11e79bb7cf4adc554d53; uccid=baeee58fe4d1d697092e61f6525e8719; ssuid=6805162414; aliyungf_tc=AQAAAOsOUQId4QcAlaRf3mqAPMUDMG/2; csrfToken=S2nttCpDrr4WCbvLkQRClEUt; bannerFlag=true; _csrf=i6MDX6NEr+KEpAxRAcWeaA==; OA=cxAohDKsDZv4yk4sQ70GtLb5KtPEhEnIp/d25AgGeuU=; _csrf_bk=76b9aab25bdab0db8930d22ee4171984; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1503634325,1504143041,1504148840,1504245847; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1504490343',
+        'Host': 'www.tianyancha.com',
+        'Referer': 'https://www.tianyancha.com/',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla / 5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+    }
+
+    html = requests.get(url, proxies=proxies, headers=headers)
+    return html
 
 def do_keyword(keyword):
     while True:
@@ -683,6 +669,32 @@ def do_keyword(keyword):
                         continue
         break
 
+def do_search_keyword(keyword):
+    while True:
+        try:
+            proxies2 = get_proxy()
+            url = 'https://www.tianyancha.com/search?key=' + urllib.quote(keyword.encode('utf8')) + '&checkFrom=searchBox'
+            print url
+            headers = {
+                'Cookie': 'TYCID=8c420960894b11e79bb7cf4adc554d53; uccid=baeee58fe4d1d697092e61f6525e8719; ssuid=6805162414; aliyungf_tc=AQAAAOsOUQId4QcAlaRf3mqAPMUDMG/2; csrfToken=S2nttCpDrr4WCbvLkQRClEUt; bannerFlag=true; _csrf=i6MDX6NEr+KEpAxRAcWeaA==; OA=cxAohDKsDZv4yk4sQ70GtLb5KtPEhEnIp/d25AgGeuU=; _csrf_bk=76b9aab25bdab0db8930d22ee4171984; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1503634325,1504143041,1504148840,1504245847; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1504490343',
+                'Host': 'www.tianyancha.com',
+                'Referer': 'https://www.tianyancha.com/',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla / 5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+            }
+            html = requests.get(url, proxies=proxies2, headers=headers)
+            # print html.text
+            if html.text.__contains__('https://static.tianyancha.com/wap/images/notFound.png'):
+                urls_result = ['-1']
+                print u'nofound.img'
+            else:
+                urls_result = re.findall('<div class="search_right_item"><div><a href="(.*?)"', html.text, re.S)
+            return urls_result
+            print 'got it'
+            break
+        except Exception,e:
+            print str(e)
+            continue
 
 def get_need_word():
     searched_list = []
@@ -707,7 +719,6 @@ def get_need_word():
             to_search_list.append(item)
 
     return to_search_list
-
 
 def main(to_search_list):
     global keyword
